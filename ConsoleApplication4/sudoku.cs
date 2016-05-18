5933b32a4fd2ff90711f1ddc34d9992caf0fd0d7
Jim Bloemkolk <Jim Bloemkolk>
2016-05-18 12:45:19 +0200

    Hij is te compilen, maar geeft nog geen snel antwoord voor de 25x25 sudoku

--- ConsoleApplication4/sudoku.cs
+++ ConsoleApplication4/sudoku.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication4
 {
@@ -14,11 +9,17 @@ namespace ConsoleApplication4
         public int y;
         public int val;
 
-        public Operation(int _x, int _y, int _val)
+        /// <summary>
+        /// zero if nothing was choosen yet, one if last choice was the best, two is last choicse was second best, etc..
+        /// </summary>
+        public int lastChoosenBest;
+
+        public Operation(int _x, int _y, int _val, int _lastChoosenBest)
         {
             x = _x;
             y = _y;
             val = _val;
+            lastChoosenBest = _lastChoosenBest;
         }
     }
 
@@ -31,7 +32,7 @@ namespace ConsoleApplication4
         {
             sudoku = new Sudoku(input);
             sudoku.PrintSudoku();
-            Solve(new Operation(0,0,0));
+            Solve(new Operation(0,0,0,0));
             return sudoku;
         }
 
@@ -75,6 +76,11 @@ namespace ConsoleApplication4
         private int n;
         private int[,] puzzle;
         private int sqrtN;
+
+        /// <summary>
+        /// Array that holds the number of possible digits that are still a valid option for each coördinate
+        /// </summary>
+        private int[,] possibleEntries;
         
         public Sudoku(string[] input)
         {
@@ -91,8 +97,63 @@ namespace ConsoleApplication4
                 }
             }
             sqrtN = Convert.ToInt32(Math.Sqrt(n));
+            ConstructPossibleEntries();
         }
 
+        /// <summary>
+        /// Generates the possibleEntries array from scratch
+        /// </summary>
+        public void ConstructPossibleEntries()
+        {
+            possibleEntries = new int[n, n];
+            for (int x = 0; x < n; x++)
+            {
+                for (int y = 0; y < n; y++)
+                {
+                    possibleEntries[x, y] = FindPossibleEntries(x,y);
+                }
+            }       
+        }
+
+        /// <summary>
+        /// Finds possible entries for a given coördinate
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private int FindPossibleEntries(int x, int y)
+        {
+            bool[] bools = new bool[n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                bools[puzzle[x, i]] = true;
+                bools[puzzle[i, x]] = true;
+            }
+            int num = n + 1;
+            for (int i = 1; i < n + 1; i++)
+            {
+                if (bools[i]) num--;
+            }
+            return num;
+        }
+
+        /// <summary>
+        /// Subtracts 1 from the column and row of coördinate (x,y). Only call this method when a puzzle entrie is changed from 0 to i in 1..n, where the resulting puzzle is a valid sudoku.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        private void UpdatePossibleEntries(int x, int y)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                possibleEntries[x, i]--;
+                possibleEntries[i, y]--;
+            }
+        }
+
+        /// <summary>
+        /// Prints the puzzle.
+        /// </summary>
         public void PrintSudoku()
         {
             for (int i = 0; i < n; i++)
@@ -108,12 +169,19 @@ namespace ConsoleApplication4
 
         public Operation GetNewSudoku(Operation lastOperation)
         {
-            int x = lastOperation.x;
-            int y = lastOperation.y;
-            int val = lastOperation.val;
-            bool found = false;
-            while (!found)
+            int lastChoosenBest = lastOperation.lastChoosenBest;
+            while (true)
             {
+                lastChoosenBest++;
+                int[] c = FindKBestPossibleEntrie(lastChoosenBest);
+                if (c[0] == -1 || c[1] == -1) return new Operation(0, 0, -1, 0); //Branch dead
+                if (c[0] == -2 || c[1] == -2) return new Operation(0, 0, -2, 0); //Answer found
+                for (int val = 0; val < n; val++)
+                {
+                    if (TestOperation(c[0], c[1], val)) return new Operation(c[0], c[1], val, lastChoosenBest);
+                }
+               
+                /*
                 if (val < n && puzzle[x, y] == 0)
                 {
                     val++;
@@ -142,13 +210,59 @@ namespace ConsoleApplication4
                 {
                     return new Operation(0,0,-2); // Answer found
                 }
+                */
             }
-            return new Operation(x,y,val);
+
+           
         }
 
+        /// <summary>
+        /// SLOW method of finding k best possible entrie
+        /// </summary>
+        /// <param name="k">1..N^2</param>
+        /// <returns>array with two elements: [0] = x-coördinate, [1] = y-coördinate</returns>
+        private int[] FindKBestPossibleEntrie(int k)
+        {
+            int[][] bestEntries =  new int[k][];
+            for (int x = 0; x < n; x++)
+            {
+                for (int y = 0; y < n; y++)
+                {
+                    for (int i = 0; i < k; i++)
+                    {
+                        if (bestEntries[i] == null)
+                        {
+                            bestEntries[i] = new[] {x, y};
+                            break;
+                        }
+                        if (possibleEntries[bestEntries[i][0], bestEntries[i][1]] >= possibleEntries[x, y])
+                        {
+                            //shift array:
+                            for (int j = k - 1; j > i; j--)
+                            {
+                                if (bestEntries[j] == null) continue;
+                                bestEntries[j] = bestEntries[j - 1];
+                            }
+                            //insert into array:
+                            bestEntries[i][0] = x;
+                            bestEntries[i][1] = y;
+                        }
+                    }
+                }
+            }
+            if (possibleEntries[bestEntries[0][0], bestEntries[0][1]] == 0) return new [] { -2, -2 }; //Answer found if best answer is zero
+            if (possibleEntries[bestEntries[k - 1][0], bestEntries[k - 1][1]] == 0) return new[] { -1, -1 }; //Branch dead if best answer is not zero, but k best answer is zero.
+            return bestEntries[k - 1];
+        }
+
+        /// <summary>
+        /// Applies operation to puzzle and updates the possibleEntries array. Only call this method when resulting puzzle is a valid sudoku.
+        /// </summary>
+        /// <param name="opp">Operation done on the sudoku</param>
         public void AugmentSudoku(Operation opp)
         {
             puzzle[opp.x, opp.y] = opp.val;
+            UpdatePossibleEntries(opp.x, opp.y);
         }
 
         private bool TestOperation(int x, int y, int val)
